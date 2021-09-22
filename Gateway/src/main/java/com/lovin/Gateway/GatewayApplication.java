4941da6620f11e3b65ca88dfc93c5e6947a3a045
jdlovin <josh.lovin@revature.net>
2021-09-22 17:48:29 -0400

    YML files

--- Gateway/src/main/java/com/lovin/Gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/lovin/Gateway/GatewayApplication.java
@@ -1,9 +1,11 @@
-package main.java.com.lovin.Gateway;
+package com.lovin.Gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayApplication {
 
 	public static void main(String[] args) {
