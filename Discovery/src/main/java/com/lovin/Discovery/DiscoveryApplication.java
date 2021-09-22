f5848031a5775a6f170f5877b5665d5cb0dd05d5
jdlovin <josh.lovin@revature.net>
2021-09-22 18:23:10 -0400

    Added Discovery module

--- Discovery/src/main/java/com/lovin/Discovery/DiscoveryApplication.java
+++ Discovery/src/main/java/com/lovin/Discovery/DiscoveryApplication.java
@@ -2,8 +2,10 @@ package com.lovin.Discovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
