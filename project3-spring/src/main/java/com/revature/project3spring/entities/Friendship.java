b61d5dc7e35287c5e3e6f22063d34787e76c12ab
PhilatPlay <philip.polo@yahoo.com>
2021-09-23 16:53:04 -0500

    Update Friendship.java
    Added Friendship Model/Entity

--- project3-spring/src/main/java/com/revature/project3spring/entities/Friendship.java
+++ project3-spring/src/main/java/com/revature/project3spring/entities/Friendship.java
@@ -22,10 +22,63 @@ import java.sql.Date;
 public class Friendship {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long friendship_Id;
-    private Long friend1_Id;
-    private Long friend2_Id;
+    private Long friendship_id;
+    private Long friend1_id;
+    private Long friend2_id;
     private String status;
     private Date date;
+    
+// could'nt get posting without adding the below
+    
+	public Friendship() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public Friendship(Long friendship_id, Long friend1_id, Long friend2_id, String status, Date date) {
+		super();
+		this.friendship_id = friendship_id;
+		this.friend1_id = friend1_id;
+		this.friend2_id = friend2_id;
+		this.status = status;
+		this.date = date;
+	}
+	public Long getFriendship_id() {
+		return friendship_id;
+	}
+	public void setFriendship_id(Long friendship_id) {
+		this.friendship_id = friendship_id;
+	}
+	public Long getFriend1_id() {
+		return friend1_id;
+	}
+	public void setFriend1_id(Long friend1_id) {
+		this.friend1_id = friend1_id;
+	}
+	public Long getFriend2_id() {
+		return friend2_id;
+	}
+	public void setFriend2_id(Long friend2_id) {
+		this.friend2_id = friend2_id;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	@Override
+	public String toString() {
+		return "Friendship [friendship_id=" + friendship_id + ", friend1_id=" + friend1_id + ", friend2_id="
+				+ friend2_id + ", status=" + status + ", date=" + date + "]";
+	}
+	
 
 }
