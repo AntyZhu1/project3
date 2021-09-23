aa8bc5d105a35ebf7b18b33d242f1c64f277f12a
PhilatPlay <philip.polo@yahoo.com>
2021-09-23 16:55:01 -0500

    Update FriendshipRepository.java
    Repository Final for Now

--- project3-spring/src/main/java/com/revature/project3spring/repositories/FriendshipRepository.java
+++ project3-spring/src/main/java/com/revature/project3spring/repositories/FriendshipRepository.java
@@ -8,7 +8,5 @@ import com.revature.project3spring.entities.Friendship;
 @Repository
 public interface FriendshipRepository extends JpaRepository<Friendship, Long>{
 
-    Friendship findFriendshipByUsersId(long friend1_id, long friend2_id);
-
-    Friendship findByFriendship(long friend1_id, long friend2_id);
 }
+
