20bf3869424b8883bbb1d3f0b9ef8ef134fedba5
PhilatPlay <philip.polo@yahoo.com>
2021-09-23 16:49:36 -0500

    Update FriendshipController.java
    Tested Friendship functionality added

--- project3-spring/src/main/java/com/revature/project3spring/controllers/FriendshipController.java
+++ project3-spring/src/main/java/com/revature/project3spring/controllers/FriendshipController.java
@@ -30,32 +30,18 @@ public class FriendshipController {
     public Friendship saveFriendship(@RequestBody Friendship friendship)	{
         return service.saveFriendship(friendship);
     }
-
-    @GetMapping("/friendship/{id}")
-    @CrossOrigin(origins = "http://localhost:4200")
-    public Friendship getFriendshipById(@PathVariable("id") long friendshipId)	{
-        return service.getFriendshipById(friendshipId);
-    }
-
+    
     @PutMapping("/friendship/approve/{id}")
     @CrossOrigin(origins = "http://localhost:4200")
-    public Friendship approveFriendship(@PathVariable("id") long friendshipId, @RequestBody Friendship friendship)	{
-        return service.approveFriendship(friendshipId, friendship);
+    public Friendship approveFriendship(@PathVariable("id") long friendship_id, @RequestBody Friendship friendship)	{
+        return service.approveFriendship(friendship_id, friendship);
     }
 
     @PutMapping("/friendship/decline/{id}")
     @CrossOrigin(origins = "http://localhost:4200")
-    public Friendship declineFriendship(@PathVariable("id") long friendshipId, @RequestBody Friendship friendship)	{
-        return service.declineFriendship(friendshipId, friendship);
+    public Friendship declineFriendship(@PathVariable("id") long friendship_id, @RequestBody Friendship friendship)	{
+        return service.declineFriendship(friendship_id, friendship);
     }
-
-    @GetMapping("/friendship/{friend1_id}/{friend2_id}")
-    public Boolean FriendshipExists(@PathVariable("id") long friend1_id, @PathVariable("id") long friend2_id) {
-        return service.friendshipExists(friend1_id, friend2_id);
-    }
-
-    @GetMapping("/friendshipapproved/friendship/{friend1_id}/{friend2_id}")
-    public Friendship findFriendshipByUsersId(@PathVariable("id") long friend1_id, @PathVariable("id") long friend2_id) {
-        return service.getFriendship(friend1_id, friend2_id);
-    }
+    
+    
 }
