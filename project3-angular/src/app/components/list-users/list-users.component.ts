04bf2ae4aa6176bc37605546124b9f370daad77b
TimBedfordRevature <timothy.bedford@revature.net>
2021-09-23 12:38:17 -0400

    Added functionality to dynamically populate list-users component

--- project3-angular/src/app/components/list-users/list-users.component.ts
+++ project3-angular/src/app/components/list-users/list-users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-list-users',
@@ -8,14 +9,18 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ListUsersComponent implements OnInit {
 
-  public searchString: string|null;
+  public searchString: any;
 
-  constructor(private route: ActivatedRoute) { 
+  public users: any;
+
+  constructor(private route: ActivatedRoute, private userService: UserService) { 
     this.searchString = route.snapshot.paramMap.get('Username');
     console.log(this.searchString);
+    this.users = this.userService.getUsersByUsername(this.searchString).subscribe((data) => {this.users = data});
   }
 
   ngOnInit(): void {
+  
   }
 
 }
